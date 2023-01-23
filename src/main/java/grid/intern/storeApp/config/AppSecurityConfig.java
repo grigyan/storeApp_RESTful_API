0001d9a7dd165c0b8d8bb4537779befc5f2ae71d
Grigor Nalbandyan <grigornalbandyan30@gmail.com>
2023-01-23 21:13:36 +0400

    removed unnecessary code

--- src/main/java/grid/intern/storeApp/config/AppSecurityConfig.java
+++ src/main/java/grid/intern/storeApp/config/AppSecurityConfig.java
@@ -1,16 +1,9 @@
 package grid.intern.storeApp.config;
 
-import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.session.web.http.CookieHttpSessionIdResolver;
 import org.springframework.session.web.http.DefaultCookieSerializer;
@@ -25,15 +18,7 @@ public class AppSecurityConfig {
         http
                 .csrf().disable()
                 .headers().frameOptions().disable();
-//                .and()
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .authorizeHttpRequests()
-//                .requestMatchers("/customers/login",
-//                        "/customers/signup", "/customers/", "/products/").permitAll()
-//                .anyRequest().permitAll()
-//                .and()
-//                .httpBasic();
-//
+
         return http.build();
     }
 
